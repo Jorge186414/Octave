0dc8661b64333f5248b177035e04bd9c3fbfb89e
Jorge186414 <90482210+Jorge186414@users.noreply.github.com>
2021-10-14 11:57:24 -0500

    Update LataChocolate.m
    Agregue documentacion

--- LataChocolate.m
+++ LataChocolate.m
@@ -1,2 +1,18 @@
-%Aqui iremos subiendo todos los codigos
-disp 'pongan algo para que deje subirlo'
+%Octave Script
+%School:       Tecnologico de Estudios Superiores de Jilotepec
+%Title:        Lata para envasar chocolate
+%Descripcion:  Lo que haremos sera resolver un problema en el que tendremos que encontrar las medidas
+%que minimicen los costos de la lata, es decir, el area minima que se debe emplear para cada bote
+%Author:       Jorge Miranda Zuñiga
+%Team:         1. Jorge Miranda Zuñiga
+%              2. Angel Jesus Santiago Hernández
+%              3. Angel Manuel Zarco Valerio
+%Date:         14/10/2021
+%Version:      1
+%Usage:        >>pwd
+%              >>cd Desktop
+%              >>cd Octave
+%              >>LataChocolate
+%Notes:        1. Se necesita el programa Octave, usar su linea de comando.
+%              
+%              2. https://octaveintro.readthedocs.io/en/latest/index.html
